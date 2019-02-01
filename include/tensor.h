a54c607648de0838805956314906e650b4bbd37a
Daniel Nichols <dando18studios@gmail.com>
2019-02-01 15:18:24 -0500

    udpated docs

--- include/tensor.h
+++ include/tensor.h
@@ -7,25 +7,56 @@ template <typename T>
 class tensor {
 public:
 
+	/** Initializes tensor with the given shape. Creates a new memory manager for this tensor. Defaults to the cpu.
+	 *	@param shape a vector of axis sizes
+	 */
 	tensor(std::vector<int> shape);
+
+	/** Initializes tensor with the given shape. Creates a new memory manager for this tensor. Uses the given device type and device id.
+	 * @param shape a vector of axis sizes
+	 * @param device the type of device
+	 * @param device_id the id of the device to be used
+	 */
 	tensor(std::vector<int> shape, device_t device, device_id_t device_id);
+
+	/** Initializes tensor with the given shape. Creates a new memory manager for this tensor. Sets every value of tensor to _fill_.
+	 * @param shape a vector of axis sizes
+	 * @param fill value to set every element of tensor
+	 */
 	tensor(std::vector<int> shape, T fill);
+	
+	/** Initializes tensor with the given shape, fill, device, and device id. Creates a new memory manager.
+	 * @param shape a vector of axis sizes
+	 * @param fill value to set every element of tensor
+	 * @param device device type tensor will use
+	 * @param device_id id of device to use
+	 */
 	tensor(std::vector<int> shape, T fill, device_t device, device_id_t device_id);
+
 	~tensor();
 
+	/** gets the value at the given index.
+	 * @param idx indices to retreive value from
+	 * @return the value at idx
+	 */
 	T get(const std::vector<int>& idx);
-	T set(const std::vector<int>& idx, T val);	
+
+	/** sets the value at the given index.
+	 * @param idx indices to set value at
+	 * @param val value to write into idx
+	 */
+	void set(const std::vector<int>& idx, T val);	
 	
 	memorymanager<T> get_memory_manager() { return this->mem_manager; }
 	std::vector<int> get_shape() { return this->shape; }
-	device_t get_device() { return this->device; }
+	device_t get_device_type() { return this->device; }
+	device_id_t get_device_id() { return this->device_id; }
 
 private:
 	init(std::vector<int>& shape, T fill, device_t device, device_id_t device_id);
 	int get_flattened_index(const std::vector<int>& idx);
-	void set_device();
 
-	memorymanager<T> *mem_manager;
+	memorymanager<T> *mem_manager;	/* allocated by init */
 	
 	std::vector<int> shape;	/* tensor axes (shape) */
 	device_t device;		/* device enum - CUDA or HOST */

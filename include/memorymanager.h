e265dc1efe7b067f3e790870407d2bc59a3dd11b
Daniel Nichols <dando18studios@gmail.com>
2019-02-07 18:27:19 -0500

    added doxygen function info

--- include/memorymanager.h
+++ include/memorymanager.h
@@ -32,32 +32,92 @@ public:
      */
     ~memorymanager();
 
-    error_t copy_from(memorymanager<T> src);
+    /** Copies the data from src memory manager into the pointer here. Asserts that
+     *  src and this have the same size.
+     *  @param src the memorymanager to copy data from
+     *  @return the error code (0 - no error, 1 - src ptr not allocated)
+     */
+    error_t copy_from(const memorymanager<T>& src);
+
+    /** copies memory from a host ptr into this memorymanager. will throw an error if it
+     *  reaches the end of src allocated mem before this is filled.
+     *  @param src the array to copy into this.
+     *  @return the error code (0 - good, 1 - not enough memory)
+     */
     error_t copy_from_host(T *src);
 
+    /** If MANAGED or CUDA_MANAGED this ensures that data is the same on all devices. It 
+     * will wait for any gpu kernels to finish before copying data. If HOST or DEVICE memory
+     * this does nothing.
+     * @return an error code
+     */
     error_t sync();
 
+    /** Changes the device this memory manager points to. Note that the memory type
+     *  is still the same, but the device_id will be different.
+     *  @return an error code (0 - ok)
+     */
     error_t set_device(device_t device_id);
 
+    /** Returns the value at idx. Error if idx is out of range.
+     *  @param idx index to retrieve
+     *  @return the value at index idx.
+     */
     T get(unsigned int idx);
+
+    /** Sets the value at idx to val. Error if idx is out of range.
+     *  @param idx index to set
+     *  @param val value to set at idx
+     */
     void set(unsigned int idx, T val);
 
+
+    /** returns a CUDA pointer
+     *  @return a pointer to the memory on a cuda device.
+     */
     T* get_device_ptr();
+
+    /** returns a CPU pointer to the data.
+     *  @return cpu pointer
+     */
     T* get_host_ptr();
+    
+    /** returns the managed CUDA memory.
+     *  @return pointer to data memory
+     */
     T* get_cuda_managed_ptr();
+    
+    /** Returns a pointer to whatever memory type this is using. Is not
+     *  defined for MANAGED memory type (returns NULL).
+     *  @return the data ptr
+     */
+    T* get_ptr();
 
-	private:
-        void init_device();
-        void init_host();
-        void init_managed();
-        void init_cuda_managed();
+    /** Returns the size of this memorymanager
+     * @return unsigned int  the size of this memory manager
+     */
+    unsigned int get_size() { return size; }
 
-		memory_t mem_type;
-        device_t device_id;
+private:
+
+    /** init with DEVICE parameters */
+    void init_device();
+
+    /** init with HOST parameters */
+    void init_host();
+
+    /** init with MANAGED parameters */
+    void init_managed();
+
+    /** init with CUDA_MANAGED parameters */
+    void init_cuda_managed();
+
+	memory_t mem_type;
+    device_t device_id;
         
-        unsigned int size;
-        T* host_ptr;
-        T* device_ptr;
-        T* cuda_managed_ptr;
+    unsigned int size;
+    T* host_ptr;
+    T* device_ptr;
+    T* cuda_managed_ptr;
 };
 

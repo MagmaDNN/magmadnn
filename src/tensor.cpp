d1dcf870df0502ce5a2cb775045776a017f22b95
Daniel Nichols <dando18studios@gmail.com>
2019-02-06 22:50:06 -0500

    fixed definition issues

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,27 +1,35 @@
 #include "tensor.h"
 
 
+
 template <typename T>
+tensor<T>::tensor(std::vector<int> shape) { }
+
+template <typename T>
+tensor<T>::tensor(std::vector<int> shape, device_t device_id) { }
+
+template <typename T>
+tensor<T>::tensor(std::vector<int> shape, T fill) { }
 
 
-tensor(std::vector<int> shape){ }
+template <typename T>
+tensor<T>::tensor(std::vector<int> shape, T fill, device_t device_id) { }
 
-tensor(std::vector<int> shape, device_t device, device_id_t device_id){ }
-
-tensor(std::vector<int> shape, T fill){ }
-
-
-tensor(std::vector<int> shape, T fill, device_t device, device_id_t device_id){ }
-
-~tensor(){ }
+template <typename T>
+tensor<T>::~tensor() { }
 
 
-T get(const std::vector<int>& idx){ }
+template <typename T>
+T tensor<T>::get(const std::vector<int>& idx) { }
 
 
-void set(const std::vector<int>& idx, T val){ }
+template <typename T>
+void tensor<T>::set(const std::vector<int>& idx, T val) { }
 
 
-init(std::vector<int>& shape, T fill, device_t device, device_id_t device_id){ }
-int get_flattened_index(const std::vector<int>& idx){ }
+template <typename T>
+void tensor<T>::init(std::vector<int>& shape, T fill, device_t device_id) { }
+
+template <typename T>
+int tensor<T>::get_flattened_index(const std::vector<int>& idx) { }
 

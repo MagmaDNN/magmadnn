da9b844f98fe00420b26893632c7b6555c734db3
Daniel Nichols <dando18studios@gmail.com>
2019-02-07 17:47:02 -0500

    initial testing code for memory manager

--- testing/testing_memorymanager.cpp
+++ testing/testing_memorymanager.cpp
@@ -7,7 +7,17 @@
 
 int main(int argc, char** argv) {
 
-    memorymanager<float> *mm = new memorymanager<float> (10, (memory_t) 1, 0);
+    memorymanager<float> *mm = new memorymanager<float> (10, HOST, 0);
+
+    // set values of memory manager
+    for (int i = 0; i < mm->get_size(); i++) {
+        mm->set(i, i*i * (0.333));
+    }
+
+    // print the values
+    for (int i = 0; i < mm->get_size(); i++) {
+        printf("%d: %.3f\n", i, mm->get(i));
+    }
 
     delete mm;
     return 0;

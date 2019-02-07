69cdcb1dd37508937bbb13afa7921c6abfa73705
Daniel Nichols <dando18studios@gmail.com>
2019-02-07 18:29:09 -0500

    added copy_from_host(*ptr) example to tester

--- testing/testing_memorymanager.cpp
+++ testing/testing_memorymanager.cpp
@@ -7,13 +7,25 @@
 
 int main(int argc, char** argv) {
 
-    memorymanager<float> *mm = new memorymanager<float> (10, HOST, 0);
+    int size = 4;
+
+    memorymanager<float> *mm = new memorymanager<float> (size, HOST, 0);
+    float *arr = new float[size];
 
     // set values of memory manager
     for (int i = 0; i < mm->get_size(); i++) {
         mm->set(i, i*i * (0.333));
+        arr[i] = 0.5 * i;
     }
 
+    // print the values
+    for (int i = 0; i < mm->get_size(); i++) {
+        printf("%d: %.3f\n", i, mm->get(i));
+    }
+
+    // copy in new vals
+    mm->copy_from_host(arr);
+    printf("\nnew vals:\n");
     // print the values
     for (int i = 0; i < mm->get_size(); i++) {
         printf("%d: %.3f\n", i, mm->get(i));

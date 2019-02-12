f6c89727fe88fe93c49ee91ebc23fa462af0cb9d
Dando18 <dando18studios@gmail.com>
2019-02-12 15:58:25 -0500

    updated testers for memory manager

--- testing/testing_memorymanager.cpp
+++ testing/testing_memorymanager.cpp
@@ -21,8 +21,10 @@ void test_host_copy(unsigned int size) {
 
 	for (int i = 0; i < (int) m2->get_size(); i++) 
 		assert( m1->get(i) == m2->get(i) );
-
+	
+	delete m2;
 	printf("Success!\n");
+
 
 	#ifdef _HAS_CUDA_
 	printf("\nTesting host->device copy...  ");
@@ -33,10 +35,35 @@ void test_host_copy(unsigned int size) {
 
 	printf("Success!\n");
 	delete m3;
+
+	printf("\nTesting host->managed copy...  ");
+	memorymanager<float> *m4 = new memorymanager<float> (size, MANAGED, (device_t) 0);
+
+	m4->copy_from(*m1);
+	m4->sync(false);
+
+	for (int i = 0; i < (int) m4->get_size(); i++) {
+		assert( m1->get(i) == m4->get(i) );
+	}
+	
+	delete m4;
+	printf("Success!\n");
+
+	printf("\nTesting host->cudamanaged copy...  ");
+	memorymanager<float> *m5 = new memorymanager<float> (size, CUDA_MANAGED, (device_t) 0);
+	
+	m5->copy_from(*m1);
+	m5->sync();
+
+	for (int i = 0; i < (int) m5->get_size(); i++) {
+		assert( m1->get(i) == m5->get(i) );
+	}
+
+	delete m5;
+	printf("Success!\n");
 	#endif
 
 	delete m1;
-	delete m2;
 }
 
 

ba4e32c6e25bc317bec25dd6cb7b6e66bfab689e
Daniel Nichols <dando18studios@gmail.com>
2019-02-19 13:09:34 -0500

    change operation to pointer

--- include/compute/tensor_operations.h
+++ include/compute/tensor_operations.h
@@ -15,21 +15,25 @@ namespace skepsi {
 
 
 template <typename T>
-class add_nocopy : public operation {
+class add_nocopy : public operation<T> {
 public:
-	add_nocopy(operation<T>& a, operation<T>& b) : operation<T>::operation({a, b}) {
-		assert( children.size() == 2);
+	add_nocopy(operation<T> *a, operation<T> *b) : operation<T>::operation({a, b}) {
+		assert( this->children.size() == 2);
 	}
 
 	tensor<T>* eval() {
-		tensor<T>* a = a->eval(children[0]);
-		tensor<T>* b = b->eval(children[1]);
+		tensor<T>* a = this->children[0]->eval();
+		tensor<T>* b = this->children[1]->eval();
 
 		assert( a->get_size() == b->get_size() );
 		
 		printf("adding a and b\n");
+		
+		for (unsigned int i = 0; i < a.get_size(); i++) {
+			b->set()
+		}
 	} 
-}
+};
 
 
 

d57ca537cf2d2042df91b110de04cf8ba7f40c21
Daniel Nichols <dando18studios@gmail.com>
2019-02-20 15:02:18 -0500

    added matmul op

---
+++ include/compute/matmul_op.h
@@ -0,0 +1,34 @@
+/**
+ * @file matmul_op.h
+ * @author Daniel Nichols
+ * @version 0.0.1
+ * @date 2019-02-20
+ * 
+ * @copyright Copyright (c) 2019
+ */
+#pragma once
+#include <vector>
+#include "operation.h"
+#include "tensor/tensor.h"
+
+namespace skepsi {
+namespace op {
+
+template <typename T>
+class matmul_op : public operation<T> {
+public:
+	matmul_op(operation<T>* a, operation<T>* b, bool copy=false) : a(a), b(b) {}
+
+	tensor<T>* eval();
+	
+	std::string to_string() { return "(" + a->to_string() + " * " + b->to_string() + ")"; }
+protected:
+	operation<T>* a;
+	operation<T>* b;
+};
+
+template <typename T>
+matmul_op<T>* matmul(operation<T> *a, operation<T> *b, bool copy=false);
+
+} // namespace op
+} // namespace skepsi

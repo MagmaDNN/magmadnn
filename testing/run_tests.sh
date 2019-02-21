c8cbd8b05c1ad7d4b474444b9f65de7dce804334
Daniel Nichols <dando18studios@gmail.com>
2019-02-21 10:16:27 -0500

    check for error in testing script

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -1,9 +1,12 @@
 # get the testers
-TESTING_FILES=$(cd bin && ls testing_*)
+TESTING_FILES=$(cd bin && ls)
 
 # run each tester
 for i in $TESTING_FILES; do
     printf "==== TESTING %s ====\n" "$i"
     ./bin/$i
+    if [ $? -ne 0 ]; then
+        printf "\n%s TEST ERROR!\n" "$i"
+    fi
     printf "\n"
 done

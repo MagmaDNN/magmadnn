59fb7372cbd93a48ef8fad4fc2e6af0884b88f72
Daniel Nichols <dando18studios@gmail.com>
2019-03-05 16:02:53 -0500

    integer definitions for tensor fillers

--- src/tensor/fill_internal_host.cpp
+++ src/tensor/fill_internal_host.cpp
@@ -49,7 +49,42 @@ void fill_uniform(memorymanager<T> &m, const std::vector<T>& params) {
         #endif
     }
 }
-template void fill_uniform(memorymanager<int>&, const std::vector<int>&);
+template <> void fill_uniform(memorymanager<int>& m, const std::vector<int>& params) {
+    /* define special for `int` because uniform_real_distribution is only for REAL_TYPE */
+    assert( params.size() >= 2 );
+
+    int start_val, end_val;
+    start_val = params[0];
+    end_val = params[1];
+
+    std::default_random_engine random_generator;
+    std::uniform_int_distribution<int> uniform_distribution(start_val, end_val);
+
+    switch (m.get_memory_type()) {
+        case HOST:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = uniform_distribution(random_generator);
+            break;
+
+        #if defined(_HAS_CUDA_)
+        case DEVICE:
+            // TODO replace with kernel call
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.set(i, uniform_distribution(random_generator));
+            break;
+        case MANAGED:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = uniform_distribution(random_generator);
+            m.sync(false);
+            break;
+        case CUDA_MANAGED:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = uniform_distribution(random_generator);
+            m.sync(false);
+            break;
+        #endif
+    }
+}
 template void fill_uniform(memorymanager<float>&, const std::vector<float>&);
 template void fill_uniform(memorymanager<double>&, const std::vector<double>&);
 
@@ -91,7 +126,42 @@ void fill_glorot(memorymanager<T> &m, const std::vector<T>& params) {
         #endif
     }
 }
-template void fill_glorot(memorymanager<int>&, const std::vector<int>&);
+template <> void fill_glorot(memorymanager<int>& m, const std::vector<int>& params) {
+    /* use binomial for integers */
+    assert( params.size() >= 2 );
+
+    int mean, std_dev;
+    mean = params[0];
+    std_dev = params[1];
+
+    std::default_random_engine random_generator;
+    std::binomial_distribution<int> normal_dist(mean, std_dev);
+
+    switch (m.get_memory_type()) {
+        case HOST:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = normal_dist(random_generator);
+            break;
+            
+        #if defined(_HAS_CUDA_)
+        case DEVICE:
+            // TODO replace with kernel call
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.set(i, normal_dist(random_generator));
+            break;
+        case MANAGED:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = normal_dist(random_generator);
+            m.sync(false);
+            break;
+        case CUDA_MANAGED:
+            for (unsigned int i = 0; i < m.get_size(); i++)
+                m.get_host_ptr()[i] = normal_dist(random_generator);
+            m.sync(false);
+            break;
+        #endif
+    }
+}
 template void fill_glorot(memorymanager<float>&, const std::vector<float>&);
 template void fill_glorot(memorymanager<double>&, const std::vector<double>&);
 

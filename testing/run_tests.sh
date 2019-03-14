4b12b84a32bc4af13cec9b713f7008cb6df2b04c
Daniel Nichols <dando18studios@gmail.com>
2019-03-14 08:21:12 -0400

    test error color

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -6,7 +6,7 @@ for i in $TESTING_FILES; do
     printf "==== TESTING %s ====\n" "$i"
     ./bin/$i
     if [ $? -ne 0 ]; then
-        printf "\n%s TEST ERROR!\n" "$i"
+        printf "\n%s \x1b[31m TEST ERROR! \x1b[0m \n" "$i"
     fi
     printf "\n"
 done

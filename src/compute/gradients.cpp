47a304a2c288a2cd767265046226fbfbd203f5b5
Daniel Nichols <dando18studios@gmail.com>
2019-05-21 16:35:16 -0400

    fixed bugs in gradient computation

--- src/compute/gradients.cpp
+++ src/compute/gradients.cpp
@@ -18,22 +18,26 @@ magmadnn_error_t get_grad_table(const std::vector<Operation<T> *>& vars, Operati
     Operation<T> *tmp;
 
     /* prune compute graph:
-        construct a new graph G' that only contains nodes that are ancestors of z and 
+        construct a new graph G' that only contains nodes that are ancestors of graph and 
         descendents of nodes in vars. */
     /* TODO */
 
     /* init Loss in grad table to one */
-    Operation<T> *ones = op::var<T>("__grad_loss", {1}, {ONE, {}}, graph->get_memory_type());
+    Operation<T> *ones = op::var<T>("__grad_loss", graph->get_output_shape(), {IDENTITY, {}}, graph->get_memory_type());
     table.set(graph, ones);
 
     /* compute the gradients for each variable */
-    for (typename std::vector<Operation<T> *>::iterator vit = vars.begin(); vit != vars.end(); vit++) {
-        err = internal::build_grad(*vit, graph, table, &tmp);
+    for (typename std::vector<Operation<T> *>::const_iterator vit = vars.begin(); vit != vars.end(); vit++) {
+        if (*vit != NULL) {
+            std::printf("calling build_grad on %s.\n", (*vit)->to_string().c_str());
+            err = internal::build_grad(*vit, graph, table, &tmp);
+        } else {
+            return (magmadnn_error_t) 1;
+        }
 
-        if (err != 0) { delete tmp; return err; }
+        if (err != 0) { return err; }
     }
 
-    delete tmp;
     return (magmadnn_error_t) 0;
 }
 template magmadnn_error_t get_grad_table(const std::vector<Operation<int> *>& vars, Operation<int> *graph, GradTable<int> &table);
@@ -47,26 +51,44 @@ namespace internal {
 
 template <typename T>
 magmadnn_error_t build_grad(op::Operation<T> *var, op::Operation<T> *graph, op::GradTable<T> &table, op::Operation<T> **grad) {
+    op::Operation<T> *tmp_grad, *result, *bprop, *consumer;
+    std::vector<op::Operation<T> *> bprops;
     magmadnn_error_t err;
+    std::vector<op::Operation<T> *> consumers;
+
+    /* error on null values */
+    if (var == NULL || graph == NULL || grad == NULL) return (magmadnn_error_t) 1;
 
     /* get this entry in the grad table */
     tmp_grad = table.get(var);
 
     /* if not null then we have already calculated this gradient */
     if (tmp_grad != NULL) {
+        std::printf("grad for %s already present [%s].\n", var->to_string().c_str(), tmp_grad->to_string().c_str());
         *grad = tmp_grad;
         return (magmadnn_error_t) 0;
     }
 
-    for (typename std::vector<Operation<T> *>::iterator vit = var->get_consumers().begin(); 
-        vit != var->get_consumers().end(); vit++) {
+    std::printf("%s needs grad (%lu consumer(s))\n", var->to_string().c_str(), var->get_consumers().size());
 
-        err = build_grad(*vit, graph, table, &tmp_grad);
+    /* build gradients for each consumer to this operation in order to properly calculate ours */
+    consumers = var->get_consumers();
+    for (typename std::vector<op::Operation<T> *>::iterator vit = consumers.begin(); vit != consumers.end(); vit++) {
+
+        consumer = (*vit);
+        
+        if (consumer == NULL) continue;
+
+        /* build the gradient for consumer and keep track of it in bprops */
+        std::printf("calling build_grad on %s.\n", consumer->to_string().c_str());
+        err = build_grad(consumer, graph, table, &tmp_grad);
         if (err != 0) return err;
-
-        bprops.push_back((*vit)->grad(*vit, var, tmp_grad));
+        bprop = consumer->grad(consumer, var, tmp_grad);
+        bprops.push_back(bprop);
     }
 
+    /* sum of each partial gradient is the total gradient */
+    /* TODO : no need to sum if just one */
     result = op::sum(bprops);
     table.set(var, result);
     *grad = result;

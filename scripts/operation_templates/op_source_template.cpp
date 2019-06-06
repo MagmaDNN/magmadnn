aa33249201a0b6593951e42b47d176091e053137
Daniel Nichols <dando18studios@gmail.com>
2019-06-06 11:35:36 -0400

    updated operation templates

--- scripts/operation_templates/op_source_template.cpp
+++ scripts/operation_templates/op_source_template.cpp
@@ -5,16 +5,22 @@ namespace magmadnn {
 namespace op {
 
 template <typename T>
-<#OPERATION_NAME#>Op<T>::<#OPERATION_NAME#>Op() : Operation<T>::Operation() {
+<#OPERATION_NAME#>Op<T>::<#OPERATION_NAME#>Op(Operation<T> *input, bool copy, bool needs_grad)
+: Operation<T>::Operation({input}, needs_grad), input(input), copy(copy) {
     /* setup code in here */
-    this->output_shape = /* ... */
-    this->mem_type = /* ... */
+    this->output_shape = input->get_output_shape();
+    this->mem_type = input->get_memory_type();
+    this->name = "<#OPERATION_NAME#>";
+
+    if (copy) {
+        this->output_tensor = new Tensor<T> (this->output_shape, {NONE, {}}, this->mem_type);
+    }
 }
 
 template <typename T>
-Tensor<T> *<#OPERATION_NAME#>Op<T>::_eval() {
+Tensor<T> *<#OPERATION_NAME#>Op<T>::_eval(bool recompute) {
     /* eval code in here ... */
-    return ret;
+    return this->output_tensor;
 }
 
 template <typename T>
@@ -29,12 +35,12 @@ template class <#OPERATION_NAME#>Op<double>;
 
 
 template <typename T>
-<#OPERATION_NAME#>Op<T> *<#OPERATION_NAME_LOWER#>() {
-    return new <#OPERATION_NAME#>Op<T>();
+<#OPERATION_NAME#>Op<T> *<#OPERATION_NAME_LOWER#>(Operation<T> *input, bool copy, bool needs_grad) {
+    return new <#OPERATION_NAME#>Op<T>(input, copy, needs_grad);
 }
-template <#OPERATION_NAME#>Op<int> *<#OPERATION_NAME_LOWER#>();
-template <#OPERATION_NAME#>Op<float> *<#OPERATION_NAME_LOWER#>();
-template <#OPERATION_NAME#>Op<double> *<#OPERATION_NAME_LOWER#>();
+template <#OPERATION_NAME#>Op<int> *<#OPERATION_NAME_LOWER#>(Operation<int> *input, bool copy, bool needs_grad);
+template <#OPERATION_NAME#>Op<float> *<#OPERATION_NAME_LOWER#>(Operation<float> *input, bool copy, bool needs_grad);
+template <#OPERATION_NAME#>Op<double> *<#OPERATION_NAME_LOWER#>(Operation<double> *input, bool copy, bool needs_grad);
 
 
 }   // namespace op

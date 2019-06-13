53870613dba66a0f31c3f2e322be707bacf00b4d
Sedrick Keh <sedrickkeh@gmail.com>
2019-06-13 16:09:25 -0400

    added tensor concat

---
+++ include/math/concat.h
@@ -0,0 +1,25 @@
+/**
+ * @file scalar_tensor_product.h
+ * @author Daniel Nichols
+ * @version 0.1
+ * @date 2019-06-07
+ * 
+ * @copyright Copyright (c) 2019
+ */
+#pragma once
+
+#include "tensor/tensor.h"
+
+namespace magmadnn {
+namespace math {
+
+template <typename T>
+void concat(Tensor<T> *A, Tensor<T> *B, Tensor<T> *C, unsigned int axis);
+
+#if defined(_HAS_CUDA_)
+template <typename T>
+void concat_device(Tensor<T> *A, Tensor<T> *B, Tensor<T> *C);
+#endif
+
+}
+}

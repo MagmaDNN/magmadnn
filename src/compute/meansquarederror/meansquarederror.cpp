4d90ea09cebc78ec99b11b2909b18f01fba05e12
Daniel Nichols <dando18studios@gmail.com>
2019-07-08 09:58:52 -0400

    compile mse for int, float, double

--- src/compute/meansquarederror/meansquarederror.cpp
+++ src/compute/meansquarederror/meansquarederror.cpp
@@ -16,6 +16,9 @@ template <typename T>
 Operation<T> *meansquarederror(Operation<T> *ground_truth, Operation<T> *prediction) {
     return op::scalarproduct(1.0f / ground_truth->get_output_shape(0), op::reducesum(op::pow(op::add(ground_truth, op::negative(prediction)), 2)));
 }
+template Operation<int> *meansquarederror(Operation<int> *ground_truth, Operation<int> *prediction);
+template Operation<float> *meansquarederror(Operation<float> *ground_truth, Operation<float> *prediction);
+template Operation<double> *meansquarederror(Operation<double> *ground_truth, Operation<double> *prediction);
 
 }
 }

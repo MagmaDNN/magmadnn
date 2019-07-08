9f502ee7675df9528cf3ba9e36081811d7fc870d
Daniel Nichols <dando18studios@gmail.com>
2019-07-08 09:59:12 -0400

    use a type safe scaling norm

--- src/compute/meansquarederror/meansquarederror.cpp
+++ src/compute/meansquarederror/meansquarederror.cpp
@@ -14,7 +14,8 @@ namespace op {
 
 template <typename T>
 Operation<T> *meansquarederror(Operation<T> *ground_truth, Operation<T> *prediction) {
-    return op::scalarproduct(1.0f / ground_truth->get_output_shape(0), op::reducesum(op::pow(op::add(ground_truth, op::negative(prediction)), 2)));
+    T norm = static_cast<T> (1.0 / ground_truth->get_output_shape(0));
+    return op::scalarproduct(norm, op::reducesum(op::pow(op::add(ground_truth, op::negative(prediction)), 2), 0));
 }
 template Operation<int> *meansquarederror(Operation<int> *ground_truth, Operation<int> *prediction);
 template Operation<float> *meansquarederror(Operation<float> *ground_truth, Operation<float> *prediction);
